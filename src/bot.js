e32fb49c3d34866b4cb4780c34f4091a91d83d53
Antoine Guillien <sladix@gmail.com>
2016-11-10 08:59:56 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,9 @@
 import { replyMessage, replyButton, getUser } from './facebook.js'
 import config from './../config.js'
 import { Client } from 'recastai'
+import { InsultGenerator } from './insults.js'
 
 const client = new Client(config.recastToken, config.language)
-
-var Utils = {
-  pick : function(myArray){
-    return myArray[Math.floor(Math.random() * myArray.length)];
-  }
-};
 
 let users = {};
 
@@ -80,44 +75,3 @@ function handleMessage(event) {
 module.exports = {
   handleMessage,
 }
-
-function InsultsGenerator(){
-      var gendras = ['M','F'];
-      var starters = ['Sous race','Chiure','Saloperie','Salet\u00e9','Maudite marde','Raclure','Esp\u00e8ce','Regarde moi ta face','Mais quelle tronche','Guette moi le facies'];
-      var preadjectifs = {
-        'M':['sale','petit','gros','vieux'],
-        'F':['sale','petite','grosse','vieille']
-      };
-      this.stack = '';
-      var qualif = {
-        'M':['biteux de premier cul','rince-crevette','con','cul','cassos','d\u00e9visse-burne','racle-merde','balais \u00e0 chiotte','bouffe-merde'],
-        'F':['pine d\'hu\u00eetre','couille','burne','bite sida\u00efque','micro-verge']
-      }
-
-      var postadjectifs = {
-        'M':['rachitique','moisi','flasque','mou','boutonneux','tout laid'],
-        'F':['tordue','molle','tordue','visqueuse','râpeuse','laide']
-      };
-      
-      var noms = ['fesses','nibards','clochards','slip sales','glaires','vieilles dames','chibres','queues','MST'];
-      
-      var actions = ['suceur','bouffeur','branleur','enculeur','l\u00e9cheur','violeur'];
-      
-      this.pickGendra = function(){
-        return Utils.pick(gendras);
-      }
-      this.pickAction = function(gendra){
-        var a = Utils.pick(actions);
-        if(gendra == 'F')
-          a = a.substr(0,a.length-1)+'se';
-        return a;
-      }
-      this.generate = function(){
-        var g = this.pickGendra();
-        this.stack = Utils.pick(starters)+' de '+Utils.pick(preadjectifs[g])+' '+Utils.pick(qualif[g])+' '+Utils.pick(postadjectifs[g])+' '+this.pickAction(g)+' de '+Utils.pick(noms);
-
-        return this.stack;
-      }
-
-      return this;
-    }
